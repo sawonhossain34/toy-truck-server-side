c0b10c22948365b6d979a0ef3865c1d8cc4275cb
sawonhossain34 <sawonhossain3491@gmail.com>
2023-05-20 22:04:37 +0600

    find all data db create api link add

--- index.js
+++ index.js
@@ -27,6 +27,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // const toyCollection = client.db('toytruck').collection('toys');
+
+    // app.get('/toys',async(req,res) => {
+    //   const result = await toyCollection.find().toArray();
+    //   res.send(result);
+    // })
+      const toyCollection = client.db('toyTruck').collection('toys');
+
+      app.get('/toys',async(req,res) => {
+        const result = await toyCollection.find().limit(20).toArray();
+        res.send(result);
+      })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
