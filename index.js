8a9ca2c6bc39bd47bb1d8fb0c0bd6f79bdf8e7c3
sawonhossain34 <sawonhossain3491@gmail.com>
2023-05-21 10:35:20 +0600

    toy id findOne get api create

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express =require('express');
 const cors =require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -38,6 +38,13 @@ async function run() {
 
       app.get('/toys',async(req,res) => {
         const result = await toyCollection.find().limit(20).toArray();
+        res.send(result);
+      })
+
+      app.get('/toys/:id',async(req,res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await toyCollection.findOne(query);
         res.send(result);
       })
 
